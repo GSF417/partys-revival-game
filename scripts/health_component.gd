9f51ed4ddee54506e0529614abec896f9279168a
Pedro Henrique <pedro.005henr@gmail.com>
2025-01-22 18:48:58 -0300

    Creates objects components and update level 2

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -1,9 +1,16 @@
 extends Node
 class_name HealthComponent
 
+@export var MAX_HEALTH := 2
+var health : float
+
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	health = MAX_HEALTH
+
 
 func suffer_damage():
-	get_parent().queue_free()
+	health -= 2
+	
+	if health <= 0 && get_parent().has_method("die"):
+		get_parent().die()
