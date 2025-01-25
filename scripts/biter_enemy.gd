50a2a1023eed68af431a1c5758493ae17cc09589
GSF417 <gsferreira16unifesp@gmail.com>
2025-01-25 17:56:14 -0300

    Added enemy jump

--- scripts/biter_enemy.gd
+++ scripts/biter_enemy.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 class_name BiterEnemy
 
 var SPEED = 300
+var JUMP_HEIGHT = -450
 
 var dead = false
 var moving = false
+var jumping = false
 var direction = 0
 var lookLeft = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var detection_range = $DetectionRange
 @onready var timer = $Timer
@@ -22,6 +25,10 @@ func chase(target: HeroEntity):
 		lookLeft = true
 	elif direction.x >= 0:
 		lookLeft = false
+	if direction.y < -0.4:
+		jumping = true
+	else:
+		jumping = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -31,12 +38,18 @@ func _process(delta: float) -> void:
 	if nearby_heroes > 0 and not dead:
 		moving = true
 		chase(detection_range.lastToEnter)
+	else:
+		direction = 0
 		
 func _physics_process(delta: float) -> void:
 	sprite.flip_h = lookLeft
 	if direction:
 		print(direction)
 		velocity.x = direction.x * SPEED
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	elif jumping:
+		velocity.y = JUMP_HEIGHT
 	move_and_slide()
 
 func die() -> void:
