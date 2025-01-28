4dbf067b52983bee30fcea022da5d9aeb18b87fd
GSF417 <gsferreira16unifesp@gmail.com>
2025-01-27 22:43:11 -0300

    Additions to stage 2 + Reaper enemy

--- scripts/detection_range.gd
+++ scripts/detection_range.gd
@@ -16,6 +16,7 @@ func last_to_enter() -> Node2D:
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body is HeroEntity:
+		$Timer.start()
 		if triggerable:
 			triggerable.trigger()
 		list_of_heroes.append(body)
--- scripts/reaper_enemy.gd
+++ scripts/reaper_enemy.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 @onready var detection_range = $DetectionRange
+@onready var attack_range = $AttackRange
 
 var detected_hero : bool = false
+var attacking : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +13,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if detection_range.heroes_count() > 0:
+	if attack_range.heroes_count() > 0:
+		attacking = true
+	elif detection_range.heroes_count() > 0:
 		detected_hero = true
 	else:
+		attacking = false
 		detected_hero = false
