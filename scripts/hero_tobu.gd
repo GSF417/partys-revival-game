702e63c3b2a6089ef683de8851aa60a445d49f43
GSF417 <gsferreira16unifesp@gmail.com>
2025-02-09 13:49:52 -0300

    Finished level 4

--- scripts/hero_tobu.gd
+++ scripts/hero_tobu.gd
@@ -1,11 +1,19 @@
 extends HeroEntity
 class_name HeroTobu
 
+@onready var hitbox_component = $HitboxComponent
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
 	SPEED = 225
 	jump_height = -620
+
+func _process(delta: float) -> void:
+	if velocity.y > 10:
+		hitbox_component.monitorable = true
+	else:
+		hitbox_component.monitorable = false
 
 func attack_recoil() -> void:
 	velocity.y = jump_height * 1.75
--- scripts/huge_rock.gd
+++ scripts/huge_rock.gd
@@ -1,9 +1,15 @@
 extends RigidBody2D
 
+var freed : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+	
+func _process(delta: float) -> void:
+	if freed:
+		freeze = false
 
 func trigger() -> void:
-	self.freeze = false
+	freed = true
+	print(freed)
