745c2ea346ecd973065db1fc8d8fd58616d9b0fa
GSF417 <gsferreira16unifesp@gmail.com>
2025-02-10 13:52:07 -0300

    Wizard stage finished

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -2,6 +2,9 @@ extends Node
 class_name HealthComponent
 
 @export var MAX_HEALTH : int = 2
+@export var health_bar : ProgressBar
+@export var flash_player : AnimationPlayer
+
 var health : float
 
 # Called when the node enters the scene tree for the first time.
@@ -11,10 +14,15 @@ func _ready():
 func suffer_damage():
 	var parent = get_parent()
 	health -= 2
+	if health_bar != null:
+		print("Current Health:")
+		print(health)
+		health_bar.set_health(health)
+	if flash_player != null:
+		flash_player.play("hit_flash")
 	if health > 0:
 		return
 	if parent.has_method("die"):
 		parent.die()
 	else:
 		parent.queue_free()
-		
